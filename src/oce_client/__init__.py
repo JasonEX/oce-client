c72a8b5ec0b28556db724737ead5e0e46cc1fd89
Yuqian <yuqian.yang@msn.com>
2026-09-01 08:19:58 -0700

    chore: mark public client exports explicitly

--- src/oce_client/__init__.py
+++ src/oce_client/__init__.py
@@ -2,32 +2,44 @@
 
 __version__ = "0.1.0"
 
-from .context import BlobCompatibilityError, CheckpointResetRequired, WorkspaceContext
-from .defaults import DEFAULT_API_KEY, DEFAULT_API_URL
-from .filesystem import FileAdmissionError, LocalFileSource
-from .http import OceApiError, OceHttpClient
-from .identity import Sha256BlobIdentity
-from .ignore import LayeredIgnoreMatcher
+from .context import BlobCompatibilityError as BlobCompatibilityError
+from .context import CheckpointResetRequired as CheckpointResetRequired
+from .context import WorkspaceContext as WorkspaceContext
+from .defaults import DEFAULT_API_KEY as DEFAULT_API_KEY
+from .defaults import DEFAULT_API_URL as DEFAULT_API_URL
+from .filesystem import FileAdmissionError as FileAdmissionError
+from .filesystem import LocalFileSource as LocalFileSource
+from .http import OceApiError as OceApiError
+from .http import OceHttpClient as OceHttpClient
+from .identity import Sha256BlobIdentity as Sha256BlobIdentity
+from .ignore import LayeredIgnoreMatcher as LayeredIgnoreMatcher
 from .models import (
-    BlobDelta,
-    BlobStatus,
-    BlobStatusResult,
-    BlobUpload,
-    CheckpointResult,
-    FileRecord,
-    FileStatus,
-    MissingResult,
-    RetrievalResult,
-    SyncResult,
-    UploadPlan,
-    UploadResult,
-    WorkspaceSnapshot,
+    BlobDelta as BlobDelta,
+    BlobStatus as BlobStatus,
+    BlobStatusResult as BlobStatusResult,
+    BlobUpload as BlobUpload,
+    CheckpointResult as CheckpointResult,
+    FileRecord as FileRecord,
+    FileStatus as FileStatus,
+    MissingResult as MissingResult,
+    RetrievalResult as RetrievalResult,
+    SyncResult as SyncResult,
+    UploadPlan as UploadPlan,
+    UploadResult as UploadResult,
+    WorkspaceSnapshot as WorkspaceSnapshot,
 )
-from .ports import BlobApi, BlobIdentity, FileSource, IgnoreMatcher, StateStore, Watcher
+from .ports import (
+    BlobApi as BlobApi,
+    BlobIdentity as BlobIdentity,
+    FileSource as FileSource,
+    IgnoreMatcher as IgnoreMatcher,
+    StateStore as StateStore,
+    Watcher as Watcher,
+)
 from .runtime import (
-    ClientConfigurationError,
-    ClientRuntime,
-    ClientSettings,
-    McpConfiguration,
+    ClientConfigurationError as ClientConfigurationError,
+    ClientRuntime as ClientRuntime,
+    ClientSettings as ClientSettings,
+    McpConfiguration as McpConfiguration,
 )
-from .state import SQLiteStateStore
+from .state import SQLiteStateStore as SQLiteStateStore
